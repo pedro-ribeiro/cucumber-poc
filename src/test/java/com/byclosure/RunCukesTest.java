8b06e1a2a6bdea674c1bfafbbde2784477453438
Bernardo Pinto <bernardo.pinto@byclosure.com>
2014-02-24 14:24:26 +0000

    Added report generation to a database.

--- src/test/java/com/byclosure/RunCukesTest.java
+++ src/test/java/com/byclosure/RunCukesTest.java
@@ -1,5 +1,6 @@
 package com.byclosure;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
@@ -7,6 +8,11 @@ import org.junit.runner.RunWith;
  *
  */
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json"})
+@CucumberOptions(format = {
+        "html:target/cucumber-html-report",
+        "json:target/cucumber-json-report.json",
+        "junit:target/cucumber-junit-report.xml",
+        "at.porscheinformatik.cucumber.formatter.HtmlFormatter:target/html",
+        "com.byclosure.MongoDBFormatter:target/db"})
 public class RunCukesTest {
 }
